b0169022fea4a32f855fc07928f691794bc31c9f
shivam60 <shivamchawla60@gmail.com>
2017-11-12 16:54:35 +0530

    designer-pdf-viewer

---
+++ designer-pdf-viewer.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    vector<int> h(26);
+    for(int h_i = 0; h_i < 26; h_i++){
+       cin >> h[h_i];
+    }
+    string word;
+    cin >> word;
+    int m=0;
+    for(int i=0;i<word.length();++i){
+        if(h[abs(int(word[i])-int('a'))]>m) m=h[abs(int(word[i])-int('a'))];
+    }
+    cout<<m*word.length();
+    return 0;
+}
+
