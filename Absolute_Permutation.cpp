79a7f004b76d944ee317c9912b800481ed2fd632
shivam60 <shivamchawla60@gmail.com>
2017-11-23 20:43:58 +0530

    Absolute Permutation

---
+++ Absolute_Permutation.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int main(){
+    int t,n,k,dev;
+    cin >> t;
+
+    for(int a0 = 0; a0 < t; a0++){
+        cin >> n >> k;
+        dev=n/k;
+        vector<int> pos(n);
+        if(k==0){
+                  for(int i=1;i<=n;i++)cout<<i<<" ";
+        }
+        else if ((k<=n/2)&&(n%k==0)&&(dev%2 ==0)){
+            for(int i=0;i<n;i++){
+                if((i/k)%2 == 0){
+                    pos[i] = i+k+1;
+                }
+                else{
+                    pos[i] = i-k+1;
+                }
+            }
+            for (int i = 0; i< n; i++)
+                cout << pos[i] <<" ";
+        }
+        else   cout << "-1";
+        cout <<endl;
+    }
+    return 0;
+}
